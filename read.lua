07b45391a9c70c0ad3af59c2e957040a0eaa0bf1
Kevin Böckler <kevinboeckler@gmail.com>
2025-09-22 20:27:13 +0200

    Extract read file to util

--- read.lua
+++ read.lua
@@ -1,15 +1,5 @@
-local open = io.open
+local util = require("util")
 
-local function read_file(path)
-	local file = open(path, "rb")
-	if not file then
-		return nil
-	end 
-	local content = file:read "*a" -- *a or *all reads the whole file
-	file:close()
-	return content
-end
-
-local fileContent = read_file("test.txt")
+local fileContent = util.fread("test.txt")
 print(type(fileContent))
 print(fileContent)
--- util.lua
+++ util.lua
@@ -1,9 +1,22 @@
 local NAME = ...
 
+local open = io.open
+
+local function read_file(path)
+	local file = open(path, "rb")
+	if not file then
+		return nil
+	end
+	local content = file:read "*a" -- *a or *all reads the whole file
+	file:close()
+	return content
+end
+
 local function sum(a, b)
 	return a+b
 end
 
 return {
-	sum = sum
+	sum = sum,
+	fread = read_file
 }
